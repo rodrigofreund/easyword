6c57588d77373c079e79f826ed776dcf84083872
Rodrigo Freund de Moraes <rodrigofreund@gmail.com>
2023-03-15 08:55:01 -0300

    add researtch resources

--- src/br/com/rodrigofreund/easyword/view/ViewService.java
+++ src/br/com/rodrigofreund/easyword/view/ViewService.java
@@ -2,6 +2,9 @@ package br.com.rodrigofreund.easyword.view;
 
 /*Service responsable for all screen access and management 
  * It needs to be the only acesses for the screen access
+ * https://www.baeldung.com/java-enum-simple-state-machine
+ * https://www.inf.pucrs.br/~flash/lapro2/lapro2_guiadv.pdf
+ * https://refactoring.guru/design-patterns/state/java/example
  */
 public final class ViewService {
 	
